4daabb6e8d4bb29b6b6359a30a41ec9025bd63f3
eskalona17 <eskalona_17@hotmail.com>
2020-05-06 12:25:03 +0200

    react redux added and components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import store from './store'
+import Jugadores from './components/Jugadores'
+import EquipoSeleccionado from './components/EquipoSeleccionado'
 
-function App() {
-  return (
-   <main></main>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <main>
+      <h1>Manager</h1>
+      <Jugadores />
+      <EquipoSeleccionado />
+    </main>
+  </Provider>
+);
 
 export default App;
